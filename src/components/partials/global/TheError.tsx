a797121415a12c29767e90a7a86c7382510311f1
ci7lus <7887955+ci7lus@users.noreply.github.com>
2020-02-12 15:08:40 +0900

    :art: カスタムエラーページ

---
+++ src/components/partials/global/TheError.tsx
@@ -0,0 +1,24 @@
+import Vue, { VNode } from "vue"
+import * as tsx from "vue-tsx-support"
+
+export const TheError = tsx.component({
+  name: "TheError",
+  props: {
+    error: {
+      type: Object,
+    },
+    statusCode: {
+      type: Number,
+    },
+  },
+  render(): VNode {
+    return (
+      <div class="flex-1 bg-black m-auto">
+        <div class="container max-w-screen-md m-auto p-4">
+          <div class="text-6xl">{this.statusCode}</div>
+          <p>{this.error.message}</p>
+        </div>
+      </div>
+    )
+  },
+})
---
+++ src/layouts/error.vue
@@ -0,0 +1,31 @@
+<template>
+  <TheError :error="error" :statusCode="statusCode" />
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import { TheHeader } from "~/components/partials/global/TheHeader"
+import { TheFooter } from "~/components/partials/global/TheFooter"
+import { TheError } from "~/components/partials/global/TheError"
+
+export default Vue.extend({
+  name: "Error",
+  components: { TheHeader, TheFooter, TheError },
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  computed: {
+    statusCode() {
+      return (this.error && this.error.statusCode) || 500
+    },
+  },
+  head() {
+    return {
+      title: this.error.message,
+    }
+  },
+})
+</script>

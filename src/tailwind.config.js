3a2120b6b855d6a721a23c949161e4d8331638b7
ci7lus <7887955+ci7lus@users.noreply.github.com>
2020-06-18 14:42:04 +0900

    :bug: 依存更新に伴ってpurgecssが無効になっていたバグを修正

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -5,6 +5,7 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
+  purge: false,
   theme: {
     extend: {
       screens: {
